ff82c045bec6de98b58a5d1eff62e5f2918cef53
mehatabmsls <mehatabmsls@gmail.com>
2024-05-31 23:29:23 +0530

    initial audio done bug fix 1

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,9 +1,12 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { redirect } from "next/navigation";
 import Link from "next/link";
 
 const Cards = () => {
+  useEffect(() => {
+    redirect("/audio");
+  }, []);
   const [paths, setPaths] = useState([
     // "Quran",
     // "Sunnah",
@@ -13,16 +16,11 @@ const Cards = () => {
     // "Knowledge",
     // "Seerah",
   ]);
-
   return (
     <>
-      {redirect("/audio")}
       {paths.map((item, index) => (
-        <Link href={`/${item.toLowerCase()}`}>
-          <div
-            key={index}
-            className=" bg-[rgb(32,32,32)] h-24 w-44 px-4 flex rounded-md justify-center items-center text-base cursor-pointer"
-          >
+        <Link key={index} href={`/${item.toLowerCase()}`}>
+          <div className=" bg-[rgb(32,32,32)] h-24 w-44 px-4 flex rounded-md justify-center items-center text-base cursor-pointer">
             {item}
           </div>
         </Link>
