5d336fe2d9b21f08a4651c7566b111c234aee82d
mehatabmsls <mehatabmsls@gmail.com>
2024-06-02 17:39:14 +0530

    Added Play and Resume options

--- src/app/audio/page.tsx
+++ src/app/audio/page.tsx
@@ -4,21 +4,53 @@ import surahs from "@/data/surahs";
 import { Howl, Howler } from "howler";
 import { Spline_Sans } from "next/font/google";
 import { useState } from "react";
+import useStore from "@/state/state";
+import { PuffLoader } from "react-spinners";
 
 const spline = Spline_Sans({ subsets: ["latin"] });
 
 export default function Home() {
   const [active, setActive] = useState("telugu");
   const [folder, setFolder] = useState("teluguaudio");
-  function stopAllAudio() {}
+  const setLoading = useStore((state: any) => state.setLoading);
+  const main = useStore((state: any) => state.main);
+  const currentSound = useStore((state: any) => state.currentSound);
+  const setCurrentSound = useStore((state: any) => state.setCurrentSound);
+  const setPause = useStore((state: any) => state.setPause);
+  const setCurrent = useStore((state: any) => state.setCurrent);
+  const currentId = useStore((state: any) => state.currentId);
+  const closePrevPause = useStore((state: any) => state.closePrevPause);
+
+  function pauseAudio(chapter: number) {
+    if (currentSound && currentSound.playing()) {
+      currentSound.pause();
+      setPause(chapter);
+    }
+  }
+
   function playAudio(chapter: number) {
-    Howler.stop();
-    const sound = new Howl({
-      src: `/${folder}/${chapter}.mp3`,
-      html5: true,
-    });
-    // Play the sound.
-    sound.play();
+    if (currentId && currentId === chapter) {
+      currentSound.play();
+      setPause(chapter);
+    } else {
+      setLoading(chapter);
+      Howler.stop();
+      const sound = new Howl({
+        src: `/${folder}/${chapter}.mp3`,
+        html5: true,
+        onload: () => {
+          setLoading(chapter);
+        },
+        onend: () => {
+          setPause(chapter);
+        },
+      });
+      sound.play();
+      setCurrentSound(sound);
+      closePrevPause();
+      setPause(chapter);
+      setCurrent(chapter);
+    }
   }
   return (
     <section className="text-white">
@@ -44,6 +76,8 @@ export default function Home() {
           onClick={() => {
             setActive("telugu");
             setFolder("teluguaudio");
+            closePrevPause();
+            Howler.stop();
           }}
           className={`px-4 py-2 bg-[rgb(32,32,32)] rounded-lg cursor-pointer ${
             active === "telugu" ? "bg-blue-600" : ""
@@ -55,6 +89,8 @@ export default function Home() {
           onClick={() => {
             setActive("hindi");
             setFolder("hindiaudio");
+            closePrevPause();
+            Howler.stop();
           }}
           className={`px-4 py-2 bg-[rgb(32,32,32)] rounded-lg cursor-pointer ${
             active === "hindi" ? "bg-blue-600" : ""
@@ -66,6 +102,8 @@ export default function Home() {
           onClick={() => {
             setActive("english");
             setFolder("englishaudio");
+            closePrevPause();
+            Howler.stop();
           }}
           className={`px-4 py-2 bg-[rgb(32,32,32)] rounded-lg cursor-pointer ${
             active === "english" ? "bg-blue-600" : ""
@@ -75,7 +113,7 @@ export default function Home() {
         </div>
       </section>
       <section className="flex flex-wrap py-6 mx-9 gap-6 justify-center my-12">
-        {surahs.map((item, index) => (
+        {main.map((item: any, index: number) => (
           <section
             key={index}
             className="w-96 flex p-4 gap-6 bg-[rgb(32,32,32)] rounded-md justify-between"
@@ -100,36 +138,40 @@ export default function Home() {
             </section>
             <section className="flex flex-col justify-center gap-2 bg-[rgb(32,32,32)]">
               {/* <div className="bg-[rgb(32,32,32)]">time</div> */}
-              <div
-                onClick={() => playAudio(item.chapter)}
-                className="bg-[rgb(32,32,32)]"
-              >
-                {/* <svg
+              <div className="bg-[rgb(32,32,32)]">
+                {item.loading ? (
+                  <PuffLoader color="#36d7b7" size={40} />
+                ) : item.paused ? (
+                  <svg
+                    onClick={() => pauseAudio(item.chapter)}
                     xmlns="http://www.w3.org/2000/svg"
-                    fill="rgb(0,0,0)"
+                    fill="none"
                     viewBox="0 0 24 24"
                     strokeWidth={1}
-                    stroke="black"
-                    className="size-9 bg-[rgb(32,32,32)] cursor-pointer"
+                    stroke="currentColor"
+                    className="size-10 bg-[rgb(32,32,32)] cursor-pointer"
                   >
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
-                      d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
+                      d="M14.25 9v6m-4.5 0V9M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
                     />
-                  </svg> */}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="size-10 bg-[rgb(32,32,32)] cursor-pointer"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+                  </svg>
+                ) : (
+                  <svg
+                    onClick={() => playAudio(item.chapter)}
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="size-10 bg-[rgb(32,32,32)] cursor-pointer"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                )}
               </div>
             </section>
           </section>
